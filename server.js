506c5a7217becc81f904b012948f9d7a15f6761c
Amanda-s-santos <amandadasilvafutura1@gmail.com>
2026-05-07 19:13:10 -0300

    ajuste conexao aiven

--- server.js
+++ server.js
@@ -11,9 +11,13 @@ app.use(express.json({ limit: "50mb" }));
 
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 db.connect((err) => {
